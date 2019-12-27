6c1d59fa97bd67d346a0f579b2f77f3408f4410a
Said Akhmedbayev <said.akh@gmail.com>
2019-12-27 12:34:31 +0600

    chore(all): configured webhook

--- webhook-config.js
+++ webhook-config.js
@@ -1,2 +1,2 @@
-const webhookUrl = "https://www.yourwebhookurl.com"
+const webhookUrl = "https://webhooks.amplify.us-east-1.amazonaws.com/prod/webhooks?id=6576548d-2466-43ca-a7d4-8f0487f47d2d&token=V7GbFFDVuDAIUlrpjpNE74k7SDPit5cBeO6iAM24"
 module.exports = webhookUrl
